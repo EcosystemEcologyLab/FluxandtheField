e334957885a96225a12a1cc405094664a1c06bb6
Lindsey Bell <lindseybell@snrenet.arizona.edu>
2025-06-11 15:28:54 -0700

    [minor] combine plots

--- Code/HeightError.R
+++ Code/HeightError.R
@@ -2,6 +2,7 @@
 
 library(dplyr)
 library(ggplot2)
+library(patchwork)
 
 dat <-  read.csv("X:/moore/FieldData/DataSpreadsheets/US-SRG_BiometGrad_07062025.csv")
 
@@ -14,13 +15,15 @@ height_summary <- dat %>%
 height_summary <- height_summary %>%
   mutate(ID = factor(ID, levels = ID[order(mean_height)]))
 
-ggplot(height_summary, aes(x = ID, y = mean_height)) +
+p1 <- ggplot(height_summary, aes(x = ID, y = mean_height)) +
   geom_bar(stat = "identity", fill = "steelblue") +
   geom_errorbar(aes(ymin = mean_height - se_height, ymax = mean_height + se_height),
                 width = 0.2) +
   labs(x = "Tree ID #", y = "Average Canopy Height (m)",
        title = "") +
-  theme_minimal()
+  theme_minimal()+
+  theme(axis.title = element_text(size = 16),
+        axis.text = element_text(size = 13))
 
 
 
@@ -33,13 +36,15 @@ CanDi_summary <- dat %>%
 CanDi_summary <- CanDi_summary %>%
   mutate(ID = factor(ID, levels = ID[order(mean_di)]))
 
-ggplot(CanDi_summary, aes(x = ID, y = mean_di)) +
+p2 <- ggplot(CanDi_summary, aes(x = ID, y = mean_di)) +
   geom_bar(stat = "identity", fill = "steelblue") +
   geom_errorbar(aes(ymin = mean_di - se_di, ymax = mean_di + se_di),
                 width = 0.2) +
   labs(x = "Tree ID #", y = "Average Canopy Diameter (m)",
        title = "") +
-  theme_minimal()
+  theme_minimal()+
+  theme(axis.title = element_text(size = 16),
+        axis.text = element_text(size = 13))
 
 
 BasDi_summary <- dat %>%
@@ -51,10 +56,15 @@ BasDi_summary <- dat %>%
 BasDi_summary <- BasDi_summary %>%
   mutate(ID = factor(ID, levels = ID[order(mean_di)]))
 
-ggplot(BasDi_summary, aes(x = ID, y = mean_di)) +
+p3 <- ggplot(BasDi_summary, aes(x = ID, y = mean_di)) +
   geom_bar(stat = "identity", fill = "steelblue") +
   geom_errorbar(aes(ymin = mean_di - se_di, ymax = mean_di + se_di),
                 width = 0.2) +
   labs(x = "Tree ID #", y = "Average Basal Diameter (m)",
        title = "") +
-  theme_minimal()
+  theme_minimal()+
+  theme(axis.title = element_text(size = 16),
+        axis.text = element_text(size = 13))
+
+
+p1+p2+p3
